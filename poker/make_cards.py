a128028aad3dce2b192ecdda8057239eb644a0cb
mariafuu <maria.fuglemsmo@gmail.com>
2016-01-29 11:24:56 +0100

    tror jeg har lagt til mapper...

---
+++ poker/make_cards.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import random
+
+class Card:
+    RANKS = (2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14)
+    SUITS = ('Spades', 'Hearts', 'Cloves', 'Diamonds')
+    
+def _init_ (self, rank, suit):
+    self._rank = rank
+    self._suit = suit
+    
+def getRank (self):
+    return self._rank
