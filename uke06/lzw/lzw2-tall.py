20265bd88a6fc2069622641d1f8c2f47d2be8050
rebeci <sjakalhodet@gmx.com>
2016-02-12 14:26:01 +0100

    lzw

---
+++ uke06/lzw/lzw2-tall.py
@@ -0,0 +1,38 @@
+def code():
+    '''
+    Implements an initial table for LZW algorithm 
+    '''
+    table = {}
+    table[1] = 'a'
+    table[2] = 'b'
+    table[3] = 'c'   
+    
+    return table
+
+def encode(message):
+    table = code()
+    string = ""
+    code_for_string = []
+    for byte in message:
+        symbol = byte
+        if (string + symbol) in table.values():
+            string = string + symbol
+        else:
+            for k,v in table.iteritems():
+                if v == string:
+                    code_for_string.append(k)
+            table[max(table.keys())+1] = string + symbol
+            string = symbol
+    for k,v in table.iteritems():
+        if v == string:
+            code_for_string.append(k)
+    print table
+    return code_for_string
+    
+    
+
+def test():
+    test_message = '233134510116101'
+    print encode(test_message)
+    
+test()
