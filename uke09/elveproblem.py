fd360c795bfa13d2e5040c68f78fd57b08fc36ed
mariafuu <maria.fuglemsmo@gmail.com>
2016-03-13 22:11:16 +0100

    Update elveproblem.py

--- uke09/elveproblem.py
+++ uke09/elveproblem.py
@@ -1,72 +1,67 @@
 # -*- coding: utf-8 -*-
 
-leftSide = ["H", "K", "R"]
-rightSide = ["empty"]
-inventory = ["empty"]
+#Setter opp hvordan spillet starter
+leftSide = ["Høne", "Korn", "Rev"]
+rightSide = []
+inventory = []
 boat = 0
+frakt = "ingenting"
 
+def start():
+    print "En mann skal flytte en rev, ei høne og en sekk med korn fra en side av en elv"
+    print "til den andre."
+    print "Mannen har en båt som kan bære ham og en av de andre."
+    print "Kornsekken og høna kan ikke være alene, fordi høna spiser kornet."
+    move()
+
+def move():
+    print "På venstre side er: " + ', '.join(leftSide)
+    print "På høyre side er: " + ', '.join(rightSide)    
+    global frakt
+    frakt = input("Hva skal mannen frakte? ")
+    if frakt == "Høne" or "høne":
+        load()
+    elif frakt == "Korn" or "Rev":
+        print "Dette valget gjør at kornet eller høna blir spist."
+        exit()
+    else:
+        print "Du har skrevet noe som er feil. Too bad for you."
+        exit()
+        
+def load():
+    #Laster opp i båten. Fjerner den valgte gjenstanden fra elvesiden og opp i båten
+    #Printer ut hva som er flyttet til båten og hva som er på hver side
+    global frakt
+    leftSide.remove(frakt)
+    inventory.append(frakt)
+    print "Du har flyttet " + frakt + " til båten."
+    if len(leftSide) == 0:
+        print 'Det står ingenting på venstre side.'  
+    else:
+        print "På venstre side er: " + ', '.join(leftSide)
+    if len(rightSide) == 0:
+            print 'Det står ingenting på høyre side.'    
+    else:
+        print "På høyre side er: " + ', '.join(rightSide)
+    sail()
+    
 def sail():
+    #Flytter båten fra en side av elven til den andre
+    global boat
     if boat == 0:
         boat = 1
     else:
         boat = 0
+    unload()
+    
+def unload():
+    global frakt
+    global boat
+    inventory.remove(frakt)
+    if boat == 0:
+        leftSide.append(frakt)
+    else:
+        rightSide.append(frakt)
+    move()
         
-def load(move):
-    leftSide.remove(move)
-    rightSide.append
-    print ', '.join(rightSide)
-    print ', '.join(leftSide)
-    
-
-print "På høyre side av elva står en mann, en rev, en høne, og en kornsekk."
-print "Mannen har en båt som kan bære ham og en av de andre."
-print "Høna og reven kan ikke være alene, fordi reven spiser høna."
-print "Kornsekken og høna kan ikke være alene, fordi høna spiser kornet."
-print "H for Høne, R for Rev, K for Korn."
-
-move = input("Hvem skal han frakte først? ")
-if move == "H":
-    load(move)
-elif move == "K" or "R":
-    print "Dette valget gjør at kornet eller høna blir spist."
-    exit()
-else:
-    print "Du har skrevet noe som er feil."
-    exit()
-
-
-
-"""
-if runde1 == "H":
-    sail()
-elif runde1 == "K" or "R"
-    print ""
-    
-Load(Sted, item)
-    “””Flytter objekt fra Sted (høyre/venstre) til båt”””
-    Innhold = innhold + item
-    Side = side – item
-    If side == Høne/Rev
-        print “You idiot. Høna e spist.”
-        Exit()
-    Elif side == Høne/Korn
-        print “No corn for you!”
-        Exit()
-    Else
-        sail()
-Unload(Sted)
-    “””Flytter objekt fra båt til Sted (høyre/venstre)”””
-    Sted = sted + innhold
-    Innhold = innhold – innhold
-    If side == Høne/Rev
-        print “You idiot. Høna e spist.”
-        Exit()
-    Elif side == Høne/Korn
-        print “No corn for you!”
-        Exit()
-    Elif 	“Do you want to load?”
-        If yes
-            load()
-    Else
-        sail()
-"""
+start(
